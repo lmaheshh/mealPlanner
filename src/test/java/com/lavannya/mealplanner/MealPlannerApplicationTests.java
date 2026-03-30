986d2694982998bf2234d0ffcc5206640108d504
lmaheshh <lavannya.mahesh@gmail.com>
2026-03-30 13:18:45 -0400

    init commit

---
+++ src/test/java/com/lavannya/mealplanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavannya.mealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
